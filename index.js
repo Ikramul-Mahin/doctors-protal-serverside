bee8f93b69874b3f84369c51bae78f9f96ab8dc4
Ikramul-Mahin <ikramul.mahin.islam@gmail.com>
2023-11-15 11:55:27 +0600

    version upodated

--- index.js
+++ index.js
@@ -42,6 +42,17 @@ async function run() {
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body
+            console.log(booking);
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+            if (alreadyBooked.length) {
+                const message = `You already booked on ${booking.appointmentDate}`
+                return res.send({ acknowledge: false, message })
+            }
             const result = await bookingsCollection.insertOne(booking)
             console.log(result);
             res.send(result)
